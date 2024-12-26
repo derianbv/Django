c6a8ea6307f67d4b106d5c1b2a574663390b2efb
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-26 16:19:11 -0500

    Create views_y_URL.py

---
+++ views_y_URL.py
@@ -0,0 +1,45 @@
+############################ VISTAS ##################################################################################33
+"""
+URL configuration for firstproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+myproject/
+├── manage.py
+├── myproject/
+│   ├── __init__.py
+│   ├── settings.py
+│   ├── urls.py
+│   └── wsgi.py
+└── myapp/
+    ├── __init__.py
+    ├── admin.py
+    ├── apps.py
+    ├── migrations/
+    │   └── __init__.py
+    ├── models.py
+    ├── tests.py
+    └── views.pyExamples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('firstapp/', include('firstapp.urls')),
+]
+
+
+
+
