9ba6b44757f0c90ad6e9b6fd77e9845548e51d46
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-26 16:11:57 -0500

    Update entornoVirtualySetting.py

--- entornoVirtualySetting.py
+++ entornoVirtualySetting.py
@@ -31,9 +31,35 @@ pip install Django
 django-admin startproject firstproject 
 
 #Luego, instalo django e inicio mi proyecto nuevo, se crea la carpeta (first proyect) 
+#!! PRIMERO SE CREA EL PROYECYO Y LUEGO LAS APPS QUE TENGA: 
 
 
-con la sig estructura: 
+<nombre_proyecto>/
+├── <nombre_proyecto>/
+│   ├── __init__.py
+│   ├── settings.py
+│   ├── urls.py
+│   └── wsgi.py
+├── manage.py
+
+
+ahora: 
+
+python3 manage.py startapp firstapp
+
+#esto crea una app nueva dento del proyecto con estos archivos: 
+
+<nombre_app>/
+├── __init__.py
+├── admin.py
+├── apps.py
+├── migrations/
+│   └── __init__.py
+├── models.py
+├── tests.py
+└── views.py
+
+#El ejemplo completo sería: 
 
 myproject/
 ├── manage.py
@@ -52,6 +78,26 @@ myproject/
     ├── tests.py
     └── views.py
 
-    Cada archivo da una idea de qué contiene su código interno
+Los proyectos pueden tener varias apps, cada app puede manejar cosas de lógica o cosas así. 
+
+Luego para correr el server tengo que: 
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+python3 manage.py runserver
+
+
+------------------------------------------------
+Watching for file changes with StatReloader
+Performing system checks...
+
+System check identified no issues (0 silenced).
+December 26, 2024 - 21:08:31
+Django version 5.1.4, using settings 'firstproject.settings'
+Starting development server at http://127.0.0.1:8000/
+Quit the server with CONTROL-C.
+----------------------------------------------------------------
+
+
 
 '''
