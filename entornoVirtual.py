da257f69180607e72876d3d3e2731afaf5b4dfd6
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-26 00:28:41 -0500

    Update entornoVirtual.py

--- entornoVirtual.py
+++ entornoVirtual.py
@@ -1,4 +1,16 @@
 ######################### Entorno virtual ####################################
-
+'''
 Sirve para montar una maquina virtual local, es decir, otro OS en dónde puedo descargar las versiones que necesite y si rompo algo pues se rompe solo en este contexto,
 si descargo cosas de manera global puede que rompa ciertos proyectos que solo sirven con ciertas versiones de los programas 
+
+
+
+|-----------------------|
+|   Sistema Operativo   | <-- Sistema principal
+|-----------------------|
+      |      |      |
+      v      v      v
+   Entorno1 Entorno2 Entorno3  <-- Cada proyecto con sus propias dependencias
+
+   
+'''
