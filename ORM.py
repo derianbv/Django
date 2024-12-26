fc2c90258f3294ed6b694debfdbcf0872209d44c
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-26 00:01:38 -0500

    Update ORM.py

--- ORM.py
+++ ORM.py
@@ -1,1 +1,8 @@
+####################################### ORM ####################################################3
 
+'''
+La idea general se basa en manejar bases de datos con objetos (ya que son muy similar), entonces Django me deja manipular objetos y de este modo, el Object-Relational Mapping
+se encargará de traducir el codigo a SQL, sin importar cuál sea su tipo (sqlite, mysql, etc). 
+
+
+'''
