a4458cc1b2f4d178c7e17977ebf87814d465ecd3
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-26 00:24:27 -0500

    Create entornoVirtual.py

---
+++ entornoVirtual.py
@@ -0,0 +1,4 @@
+######################### Entorno virtual ####################################
+
+Sirve para montar una maquina virtual local, es decir, otro OS en dónde puedo descargar las versiones que necesite y si rompo algo pues se rompe solo en este contexto,
+si descargo cosas de manera global puede que rompa ciertos proyectos que solo sirven con ciertas versiones de los programas 
