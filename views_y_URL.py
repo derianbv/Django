4ebf81b6f69370fc3cf977aaabab02fe1d1347fa
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-26 16:42:08 -0500

    Update views_y_URL.py

--- views_y_URL.py
+++ views_y_URL.py
@@ -13,6 +13,40 @@ from django.http import HttpResponse #Esta clase ayuda a devolver una respuesta 
 
 def home(request): #request como parámetro es la solicitud del usuario
     return HttpResponse("¡Hola, mundo! Esta es la página principal de firstapp.")
+
+
+"""
+Parámetros que recibe HttpResponse(content, content_type, status, reason, charset, headers)
+
+1. content (str, bytes, or iterable):
+El contenido de la respuesta. Puede ser una cadena de texto, bytes o un iterable que genere cadenas de texto o bytes.
+Ejemplo: HttpResponse("Hello, world!")
+
+2.content_type (str, opcional):
+
+Especifica el tipo de contenido de la respuesta. Por defecto, es 'text/html'.
+Ejemplo: HttpResponse("Hello, world!", content_type="text/plain")
+
+3.status (int, opcional):
+
+El código de estado HTTP de la respuesta. Por defecto, es 200.
+Ejemplo: HttpResponse("Not Found", status=404)
+
+4. reason (str, opcional):
+
+La razón de la respuesta HTTP. Por defecto, se basa en el código de estado.
+Ejemplo: HttpResponse("Not Found", status=404, reason="Not Found")
+
+5. charset (str, opcional):
+
+El conjunto de caracteres (charset) de la respuesta. Por defecto, es 'utf-8'.
+Ejemplo: HttpResponse("Hello, world!", content_type="text/html", charset="utf-8")
+
+6.headers (dict, opcional):
+
+Un diccionario de encabezados HTTP que se agregarán a la respuesta.
+Ejemplo: HttpResponse("Hello, world!", headers={"Custom-Header": "value"})
+"""
     
 """
 3. Ahora es necesario configurar la URL de la vista en firstapp/urls.py:
@@ -39,7 +73,7 @@ Ejemplo: {'foo': 'bar'} pasará el argumento foo con el valor 'bar' a la vista.
 Ejemplo: name='post-detail'.
 
 
-4. Ahora en mi_proyecto/urls.py debo incluir las urls de mi app: 
+4. Ahora en mi proyecto: mi_proyecto/urls.py debo incluir las urls de mi app: 
 """
 
 from django.contrib import admin
