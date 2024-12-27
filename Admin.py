f759319d1cf923b3868c8458ff13a952f159255d
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2024-12-27 13:57:00 -0500

    Update Admin.py

--- Admin.py
+++ Admin.py
@@ -1,4 +1,6 @@
 ################################################  Admin ####################################################
+## EPP: Sirve para manipular la base de datos sin necesidad de usar ORM
+
 #Setearlo----------------------------------------------------------------------
 
 # 1. Create super user: 
@@ -7,3 +9,10 @@ python3 manage.py createsuperuser
 
 #2. Correr el servidor y agregar la ruta /admin al final de la url 
 python3 manage.py runserver
+
+#3. Haber creado los modelos en models.py, y luego añadirlos como modelos admin: 
+
+from django.contrib import admin
+
+admin.site.register(Course)
+admin.site.register(Instructor)
