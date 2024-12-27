9a14d24b33f2acbd9fcfb271fdae4c185024c6c2
derianbv <116598387+derianbv@users.noreply.github.com>
2024-12-26 20:58:19 -0500

    Update docker.py

--- docker.py
+++ docker.py
@@ -1,1 +1,13 @@
 ################################### docker.py #################################################
+
+######################### Setting ##########################################
+# 1. project/settings.py: 
+ALLOWED_HOSTS = ['*','.us-south.codeengine.appdomain.cloud']
+
+#2.  in project/: 
+(linux) 
+
+pip install pipreqs
+pipreqs . #busca en el directorio actual los requerimientos para correr la app y los guarda en requirements 
+
+#Esto va a crear un archivo nuevo requirements.txt que tendrá lo necesario para mi app
