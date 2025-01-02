731c18566441763842ae9992f69b0b390f4c4b0e
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-01-02 00:59:10 -0500

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -1,2 +1,21 @@
 ############################################### Autorización ############################################## 
-Todo empieza en los modelos usuarios, que gestiona parametros como username, password, first_name, last_name, email, roles, is_active 
+#Todo empieza en los modelos usuarios, que gestiona parametros como username, password, first_name, last_name, email, roles, is_active 
+
+##### Pasos ########################: 
+
+# 1. Crear un super user: 
+
+python3 manage.py createsuperuser
+
+# Se añanden al html cosas que solo aparezcan si el usuario está verificado:
+
+{% if user.is_authenticated %}
+<div class="rightalign">
+       <div class="dropdown">
+           <button class="dropbtn">{{user.first_name}}</button>
+           <div class="dropdown-content">
+                <a href="{% url 'onlinecourse:logout' %}">Logout</a>
+           </div>
+       </div>
+</div>
+{% endif %}
