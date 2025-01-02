6e1c69d2b43d428f8c281ca11d97fd96732f4303
Derian Rojas <116598387+derianbv@users.noreply.github.com>
2025-01-01 23:42:14 -0500

    Update Auth.py

--- Auth.py
+++ Auth.py
@@ -1,1 +1,2 @@
 ############################################### Autorización ############################################## 
+Todo empieza en los modelos usuarios, que gestiona parametros como username, password, first_name, last_name, email, roles, is_active 
